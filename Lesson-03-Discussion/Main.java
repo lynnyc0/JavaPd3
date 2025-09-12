1865fb3e3da5262035ae9ad4e5bc3e56856b64a2
lynnyc0 <brialynnc@nycstudents.net>
2025-09-12 13:25:13 +0000

    blah blah

--- Lesson-03-Discussion/Main.java
+++ Lesson-03-Discussion/Main.java
@@ -7,10 +7,12 @@ class Main {
   // program begins.  
 
   void init(){
-
+      System.out.println("Bria-Lynn Angelina Cipollaro-To");
+      System.out.println("20854 15th Rd");
+      System.out.println("Bria-Lynn Angelina Cipollaro-T0/n20854 15th Rd ")
       // This is how to print to the console 
       // in Java.
-      System.out.println("Hello World");
+      
     
   }
 
