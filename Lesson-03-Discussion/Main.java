c90e407f1b0c85a0b6c58bbbf9d63ed91cfdb527
lynnyc0 <brialynnc@nycstudents.net>
2025-09-25 13:31:23 +0000

    update

--- Lesson-03-Discussion/Main.java
+++ Lesson-03-Discussion/Main.java
@@ -9,7 +9,7 @@ class Main {
   void init(){
       System.out.println("Bria-Lynn Angelina Cipollaro-To");
       System.out.println("20854 15th Rd");
-      System.out.println("Bria-Lynn Angelina Cipollaro-T0/n20854 15th Rd ")
+      System.out.println("Bria-Lynn Angelina Cipollaro-T0/n20854 15th Rd");
       // This is how to print to the console 
       // in Java.
       
---
+++ Lesson-07-activity/Main.java
@@ -0,0 +1,92 @@
+
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+/*  
+    Challenge 1:
+    1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
+*/
+  System.out.println("Enter X");
+  double x = Input.readDouble();
+  double y = 0;
+  y = Math.pow(x,7);
+  System.out.println(y);
+
+
+/*  
+    Challenge 2:
+    1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
+*/
+  System.out.println("Enter X");
+  double z = Input.readDouble();
+  double q = 0;
+  q = Math.pow(z, 3) +5;
+  System.out.println(q);
+
+/*  
+    Challenge 3:
+    Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
+    
+*/
+  System.out.println("Enter t and r");
+  double t = 0;
+  double r = 0;
+  s = Math.pow(t, 5) 
+  System.out.println(q);
+ 
+
+/*  
+    Challenge 4:
+    Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
+    
+*/
+
+
+
+/*  
+    Challenge 5:
+    Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
+    
+*/
+
+
+
+
+/*  
+    Challenge 6:
+    Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
+    
+*/
+
+
+
+
+
+/*  
+    Challenge 7:
+    Create the variables, ask the user for the variable values, write the equation in file EQ5-act6 and display the equation value.
+    
+*/
+
+
+
+
+/*  
+    *** Bonus Challenge ***:
+    Create the variables, ask the user for the variable values, write the equation in file Ch-act6 and display the equation value.
+
+    HINT: What does the "plus minus: after "-b" mean.
+*/
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
