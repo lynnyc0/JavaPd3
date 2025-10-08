e4acd5308fa6fcddd69b739edeb3f27dc62d6b44
lynnyc0 <brialynnc@nycstudents.net>
2025-10-08 13:30:42 +0000

    aaaaaa

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,18 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+	double radius = 3.9;
+	double height = 1;
+	System.out.println(radius,height);
+  }
+
+  double coneVolume(double radius, double height){
+    double volume = 1.0/3 * MAth.PI * r * r * h
+    return volume;
+	}
+}
+ 
