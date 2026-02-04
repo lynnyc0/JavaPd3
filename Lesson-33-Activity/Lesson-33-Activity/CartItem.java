ec029f640806c5368ec56e1690b3a135c20bd31e
lynnyc0 <brialynnc@nycstudents.net>
2026-02-04 14:03:26 +0000

    fedswsq

---
+++ Lesson-33-Activity/Lesson-33-Activity/CartItem.java
@@ -0,0 +1,17 @@
+class CartItem{
+  String itemName;
+  double itemPrice;
+  int quantity;
+  boolean onSale;
+
+  //Create the constructor
+ 
+
+
+  
+  // write getPrice based on condition detailed in 
+  // challenges.txt
+
+
+
+}
