3371ff46a389e434453dd650952f0635db968c1c
lynnyc0 <brialynnc@nycstudents.net>
2026-02-06 03:39:25 +0000

    sddcsdc

--- Lesson-33-Activity/Lesson-33-Activity/Main.java
+++ Lesson-33-Activity/Lesson-33-Activity/Main.java
@@ -9,8 +9,37 @@ class Main {
 
   void init(){
 
+  class CartItem{
+    constructor(name, price, onSAle){
+      this.name = name;
+      this.price = price;
+      this.type = type;
+      this.onSale = onSale;
+    }
+
+    getItemPrice() {
+      if (this.onSale) {
+        return this.price * 0.99;
+      }
+    }
+  }
   
-    
+  let cart = [
+    new CartItem("Bananas", 3.49, "food", true),
+    new CartItem("Headphones", 49.99, "eletronics", false),
+    new CartItem("Notebook", 2.25, "school", true)
+  ];
+    for let(i = 0; i < cart.length; i ++){
+      let item = cart[i];
+      if (item.onSale){
+
+      }
+    }
+    let subtotal = 0;
+    for(let i = 0, i < cart.length; i++){
+      
+    }
+
   }
 
 
