c39d5918e51f65fea7436f23d2202b2747c4547a
lynnyc0 <brialynnc@nycstudents.net>
2026-02-06 03:56:22 +0000

    sdcs

--- Lesson-35-Activity/Main.java
+++ Lesson-35-Activity/Main.java
@@ -11,14 +11,62 @@ class Main {
     Student[] students = list.getStudentArray();
 
     // add your code below here
+ \\Challenge 1   
+    let passing = 0;
+    let failing = 0;
 
+    for(let i = 0; i < student.length; i++){
+      if(students[i].isPassing()){
+        passing++;
+      } else{
+        failing++;
+      }
+    }
+\\Challenge 2
+    let failCount = 0;
+
+
+
+    for (let i = 0; i < students.length; i++){
+      if(students[i].isPassing()){
+        failCount++;
+      }
+    }
     
-    
-    
+    let percentFailing = (failCOunt / students.length) * 100;
+  
   }
 
+\\Challenge 3
+  let count = 0;
+
+\\Challenge 5
+  for(let i = 0; i < students.length; i++){
+    if (students[i].isPassing() && students.[i].getTeacher() == "castro r"){
+      count ++;
+    }
+  }
+    let count = 0;
 
 
+\\challenge 4
+  for(let i = 0; i < students.length; i++){
+    if {
+      students[i].isPassing() && students[i].getCourse().startWith("UL")
+    }
+    count ++;
+
+    let count = 0;
+
+
+
+  for(let i = 0; i < students.length; i++){
+    let grade = students.[i].getGradeLevel();
+    let gpa = students[i].getGPA();
+
+    if (grade == "freshman" || grade == "sophomore") && gpa > 90) {
+      count++;
+    }
 
 
 
